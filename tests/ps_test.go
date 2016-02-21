b400d7c0904befa97cdadb9123d6b91517ef1444
Matt Boersma <mboersma@deis.com>
2016-02-21 15:55:30 -0700

    feat(ps_test): implement table-driven processes tests

--- tests/ps_test.go
+++ tests/ps_test.go
@@ -2,47 +2,65 @@ package tests
 
 import (
 	"fmt"
+	"math/rand"
 	"os"
 	"regexp"
+	"sort"
+	"strconv"
 	"strings"
+	"sync"
 
 	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/ginkgo/extensions/table"
 	. "github.com/onsi/gomega"
 	. "github.com/onsi/gomega/gbytes"
 	. "github.com/onsi/gomega/gexec"
 )
 
-var _ = Describe("deis", func() {
+// scrapeProcs returns the sorted process names for an app from the given output.
+// It matches the current "deis ps" output for a healthy container:
+//   earthy-vocalist-v2-cmd-1d73e up (v2)
+//   myapp-v16-web-bujlq up (v16)
+func scrapeProcs(app string, output []byte) []string {
+	re := regexp.MustCompile(fmt.Sprintf(`%s-v\d+-[\w-]+ up \(v\d+\)`, app))
+	found := re.FindAll(output, -1)
+	procs := make([]string, len(found))
+	for i := range found {
+		procs[i] = string(found[i])
+	}
+	sort.Strings(procs)
+	return procs
+}
 
-	var appName string
-	var appURL string
+var _ = FDescribe("Processes", func() {
 
 	Context("with a deployed app", func() {
 
+		appName := getRandAppName()
+		appURL := strings.Replace(url, "deis", appName, 1)
+		once := &sync.Once{}
+
 		BeforeEach(func() {
-			os.Chdir("example-go")
-			appName = getRandAppName()
-			appURL = strings.Replace(url, "deis", appName, 1)
-			cmd := createApp(appName)
-			Eventually(cmd).Should(SatisfyAll(
-				Say("Git remote deis added"),
-				Say("remote available at ")))
-			Eventually(cmd).Should(Exit(0))
-			cmd, err := start("GIT_SSH=%s git push deis master", gitSSH)
-			Expect(err).NotTo(HaveOccurred())
-			Eventually(cmd.Err, "2m").Should(Say("Done, %s:v2 deployed to Deis", appName))
-			Eventually(cmd).Should(Exit(0))
+			// Set up the Processes test app only once and assume the suite will clean up.
+			once.Do(func() {
+				os.Chdir("example-go")
+				cmd := createApp(appName)
+				Eventually(cmd).Should(SatisfyAll(
+					Say("Git remote deis added"),
+					Say("remote available at ")))
+				Eventually(cmd).Should(Exit(0))
+				cmd, err := start("GIT_SSH=%s git push deis master", gitSSH)
+				Expect(err).NotTo(HaveOccurred())
+				Eventually(cmd.Err, "2m").Should(Say("Done, %s:v2 deployed to Deis", appName))
+				Eventually(cmd).Should(Exit(0))
+			})
 		})
 
-		AfterEach(func() {
-			defer os.Chdir("..")
-			destroyApp(appName)
-		})
+		DescribeTable("can scale up and down",
 
-		It("can scale up and down", func() {
-
-			// loop through several scale values
-			for _, scaleTo := range []int{1, 3, 5, 1, 0, 1} {
+			func(scaleTo, respCode int) {
+				// TODO: need some way to choose between "web" and "cmd" here!
+				// scale the app's processes to the desired number
 				sess, err := start("deis ps:scale web=%d --app=%s", scaleTo, appName)
 				Expect(err).NotTo(HaveOccurred())
 				Eventually(sess).Should(Say("Scaling processes... but first,"))
@@ -60,25 +78,79 @@ var _ = Describe("deis", func() {
 				bytes := re.FindAll(stdout, -1)
 				Expect(len(bytes)).To(Equal(scaleTo), string(stdout))
 
-				if scaleTo > 0 {
-					// TODO: curl app endpoint
+				// curl the app's root URL and print just the HTTP response code
+				sess, err = start(`curl -sL -w "%{http_code}\\n" "%s" -o /dev/null`, appURL)
+				Eventually(sess).Should(Say(strconv.Itoa(respCode)))
+				Eventually(sess).Should(Exit(0))
+			},
+
+			Entry("scale to 1", 1, 200),
+			Entry("scale to 3", 3, 200),
+			Entry("scale to 0", 0, 502),
+			Entry("scale to 5", 5, 200),
+			Entry("scale to 0", 0, 502),
+			Entry("scale to 1", 1, 200),
+		)
+
+		DescribeTable("can restart processes",
+
+			func(restart string, scaleTo int, success bool, respCode int) {
+				// TODO: need some way to choose between "web" and "cmd" here!
+				// scale the app's processes to the desired number
+				sess, err := start("deis ps:scale web=%d --app=%s", scaleTo, appName)
+				Expect(err).NotTo(HaveOccurred())
+				Eventually(sess).Should(Say("Scaling processes... but first,"))
+				// Eventually(sess).Should(Say(`done in \d+s`))
+				// Eventually(sess).Should(Say("=== %s Processes", appName))
+				Eventually(sess).Should(Exit(0))
+
+				// capture the process names
+				beforeProcs := scrapeProcs(appName, sess.Out.Contents())
+
+				// restart the app's process(es)
+				var arg string
+				switch restart {
+				case "all":
+					arg = ""
+				case "by type":
+					// TODO: need some way to choose between "web" and "cmd" here!
+					arg = "web"
+				case "by wrong type":
+					// TODO: need some way to choose between "web" and "cmd" here!
+					arg = "cmd"
+				case "one":
+					arg = beforeProcs[rand.Intn(len(beforeProcs))]
 				}
-			}
-		})
+				sess, err = start("deis ps:restart %s --app=%s", arg, appName)
+				Expect(err).NotTo(HaveOccurred())
+				Eventually(sess).Should(Say("Restarting processes... but first,"))
+				// Eventually(sess).Should(Say(`done in \d+s`))
+				// Eventually(sess).Should(Say("=== %s Processes", appName))
+				Eventually(sess).Should(Exit(0))
 
-		XIt("can restart all processes", func() {
-			// "deis ps:scale web=5 --app=%s"
-			// "deis ps:list --app=%s"
-			// curl app
-			// "deis ps:restart web --app=%s"
-			// "deis ps:list --app=%s"
-			// curl app
-		})
+				// capture the process names
+				afterProcs := scrapeProcs(appName, sess.Out.Contents())
 
-		XIt("can restart a specific process", func() {
-			// "deis ps:restart web.1 --app=%s"
-			// "deis ps:list --app=%s"
-			// curl app
-		})
+				// compare the before and after sets of process names
+				Expect(beforeProcs).NotTo(Equal(afterProcs))
+
+				// curl the app's root URL and print just the HTTP response code
+				sess, err = start(`curl -sL -w "%{http_code}\\n" "%s" -o /dev/null`, appURL)
+				Eventually(sess).Should(Say(strconv.Itoa(respCode)))
+				Eventually(sess).Should(Exit(0))
+			},
+
+			Entry("restart one of 1", "one", 1, true, 200),
+			Entry("restart all of 1", "all", 1, true, 200),
+			Entry("restart all of 1 by type", "by type", 1, true, 200),
+			Entry("restart all of 1 by wrong type", "by wrong type", 1, true, 200),
+			Entry("restart one of 6", "one", 6, true, 200),
+			Entry("restart all of 6", "all", 6, true, 200),
+			Entry("restart all of 6 by type", "by type", 6, true, 200),
+			Entry("restart all of 6 by wrong type", "by wrong type", 6, true, 200),
+			Entry("restart all of 0", "all", 0, true, 502),
+			Entry("restart all of 0 by type", "by type", 0, true, 502),
+			Entry("restart all of 0 by wrong type", "by wrong type", 0, true, 502),
+		)
 	})
 })
